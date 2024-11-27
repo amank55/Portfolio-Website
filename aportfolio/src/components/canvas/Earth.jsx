9771b2e80fb665bc9efaabb7957d0aee62e7ce43
amank55 <amankumarr2005ak@gmail.com>
2024-11-27 23:13:59 +0530

    .

--- aportfolio/src/components/canvas/Earth.jsx
+++ aportfolio/src/components/canvas/Earth.jsx
@@ -1,4 +1,5 @@
-import React, { Suspense } from "react";
+import React from "react";
+import { Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 
