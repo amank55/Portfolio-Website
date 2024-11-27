9aa218813ffa7b1f457206ede3e071ded0e559c2
amank55 <amankumarr2005ak@gmail.com>
2024-11-27 15:43:15 +0530

    Made changes in vite

--- aportfolio/vite.config.js
+++ aportfolio/vite.config.js
@@ -1,24 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
+import { defineConfig } from 'vite';
+
 export default defineConfig({
-  plugins: [react()],
   build: {
-    chunkSizeWarningLimit: 2000, // Optional: to suppress the 500 KB warning
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            // Separate all node_modules into vendor chunk
-            return 'vendor';
-          }
-          // You can add more conditions for specific libraries
-          if (id.includes('src/components')) {
-            return 'components'; // Example for splitting components
-          }
-        }
-      }
-    }
-  }
+    outDir: 'dist', // Sets the output directory to 'dist'
+  },
 });
